b37f57b7d3f7859a007e3de06a2346f60c9ce782
Raj <raji.japan.wk@gmail.com>
2019-05-27 06:35:33 +0900

    Fixed login issue

--- src/app/core/api-service/api-auth-service.service.ts
+++ src/app/core/api-service/api-auth-service.service.ts
@@ -7,37 +7,39 @@ import { environment as env } from '@env/environment';
 import { catchError, filter } from 'rxjs/operators';
 import { HttpErrorResponse, HttpClient } from '@angular/common/http';
 import { Store, select } from '@ngrx/store';
-import {selectAuth } from '../auth/auth.selectors';
+import { selectAuth } from '../auth/auth.selectors';
 import { AppState } from '../core.state';
 import { ActionAuthLoginToken } from '../auth/auth.actions';
 import { AuthState } from '../auth/auth.models';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiAuthServiceService {
-
   baseURL = env.apiUrl + '/google-auth';
   token = '';
 
   constructor(private http: HttpClient, private store: Store<AppState>) {
     this.store
-        .pipe(select(selectAuth),
-              filter((authState: AuthState) => authState.token === null)
-        ) .subscribe(state => this.addAuth(state.basicProfile, state.googleAuth));
+      .pipe(
+        select(selectAuth),
+        filter(
+          (authState: AuthState) =>
+            authState.token === null && authState.isAuthenticated === true
+        )
+      )
+      .subscribe(state => this.addAuth(state.basicProfile, state.googleAuth));
   }
 
-  addAuth (basicProfile: BasicProfile, googleAuth: GoogleAuth): void {
+  addAuth(basicProfile: BasicProfile, googleAuth: GoogleAuth): void {
     const auth: Auth = {
       basicProfile,
       googleAuth,
       idKey: undefined
-    }
-    this.http.post<Auth>(this.baseURL, auth)
-      .pipe(
-        catchError(this.handleError)
-      )
+    };
+    this.http
+      .post<Auth>(this.baseURL, auth)
+      .pipe(catchError(this.handleError))
       .subscribe(result => {
         this.store.dispatch(new ActionAuthLoginToken({ token: result.idKey }));
         this.token = result.idKey;
@@ -56,13 +58,10 @@ export class ApiAuthServiceService {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong,
       console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
     }
     // return an observable with a user-facing error message
-    return throwError(
-      'Something bad happened; please try again later.');
-  };
-
-
+    return throwError('Something bad happened; please try again later.');
+  }
 }
